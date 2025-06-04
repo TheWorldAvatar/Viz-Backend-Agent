a241981d32971083bbf196a3d725731a65ccf5fa
qhouyee <quekhouyee@gmail.com>
2025-06-04 14:38:11 +0100

    qhouyee/5/shacl-data-roles: add authentication setup

---
+++ agent/src/main/java/com/cmclinnovations/agent/config/KeycloakJwtRolesConverter.java
@@ -0,0 +1,36 @@
+package com.cmclinnovations.agent.config;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+public class KeycloakJwtRolesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+    public static final String PREFIX_RESOURCE_ROLE = "ROLE_";
+
+    private static final String CLAIM_RESOURCE_ACCESS = "resource_access";
+    private static final String CLAIM_CLIENT = "viz";
+    private static final String CLAIM_ROLES = "roles";
+
+    /**
+     * Extracts the relevant client roles from a JWT token.
+     */
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Collection<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        // This agent requires only the client roles for the viz client to manage
+        // resources; All other realm or client roles are ignored
+        Map<String, Map<String, Collection<String>>> resourceAccess = jwt.getClaim(CLAIM_RESOURCE_ACCESS);
+        if (resourceAccess != null && !resourceAccess.isEmpty()) {
+            resourceAccess.get(CLAIM_CLIENT).get(CLAIM_ROLES).forEach(
+                    role -> grantedAuthorities
+                            .add(new SimpleGrantedAuthority(PREFIX_RESOURCE_ROLE + CLAIM_CLIENT + "_" + role)));
+        }
+
+        return grantedAuthorities;
+    }
+}
---
+++ agent/src/main/java/com/cmclinnovations/agent/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.cmclinnovations.agent.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.DelegatingJwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Value("${KEYCLOAK_ISSUER_URI}")
+    private String tokenIssuerUrl;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // This converter combines multiple converters; extracts the roles from the JWT
+        // token and have to be plugged into a JwtAuthenticationConverter
+        DelegatingJwtGrantedAuthoritiesConverter authoritiesConverter = new DelegatingJwtGrantedAuthoritiesConverter(
+                new JwtGrantedAuthoritiesConverter(), new KeycloakJwtRolesConverter());
+
+        // This converter converts a Jwt object into an Authentication Token
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize.requestMatchers("/status").permitAll()
+                .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation(tokenIssuerUrl);
+    }
+
+    @Bean
+    GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults("");
+    }
+}
