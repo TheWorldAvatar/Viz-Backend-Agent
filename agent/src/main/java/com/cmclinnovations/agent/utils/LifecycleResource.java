0a67ca3556d13f0564a02dddbecd7f23862ee136
qhouyee <quekhouyee@gmail.com>
2025-09-10 10:16:47 +0100

    qhouyee/51/fail-to-edit-instances: fix replacement regex with unremoved .

--- agent/src/main/java/com/cmclinnovations/agent/utils/LifecycleResource.java
+++ agent/src/main/java/com/cmclinnovations/agent/utils/LifecycleResource.java
@@ -166,7 +166,7 @@ public class LifecycleResource {
         .replaceFirst(
             "\\?iri \\<http\\:\\/\\/www\\.w3\\.org\\/1999\\/02\\/22\\-rdf\\-syntax\\-ns\\#type\\> \\/ \\<http\\:\\/\\/www\\.w3\\.org\\/2000\\/01\\/rdf\\-schema\\#subClassOf\\>\\* \\<https\\:\\/\\/spec\\.edmcouncil\\.org\\/fibo\\/ontology\\/FBC\\/ProductsAndServices\\/FinancialProductsAndServices\\/ContractLifecycleEventOccurrence\\>\\ .",
             "")
-        .replaceFirst("\\?iri dc\\-terms\\:identifier \\?id.", "")
+        .replaceFirst("\\?iri dc\\-terms\\:identifier \\?id \\.", "")
         // Replace iri with event variable
         .replace(QueryResource.IRI_VAR.getQueryString(), eventVar);
     return "OPTIONAL {" + eventVar + " <https://spec.edmcouncil.org/fibo/ontology/FND/Relations/Relations/exemplifies> "
