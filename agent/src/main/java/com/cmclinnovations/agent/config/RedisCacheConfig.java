6c2f0b236ee9df90a116019039a97488319791cd
qhouyee <quekhouyee@gmail.com>
2026-01-08 15:32:48 +0000

    qhouyee/139/redis-authentication: create a connection factory directly to set password based on docker secrets

--- agent/src/main/java/com/cmclinnovations/agent/config/RedisCacheConfig.java
+++ agent/src/main/java/com/cmclinnovations/agent/config/RedisCacheConfig.java
@@ -1,12 +1,22 @@
 package com.cmclinnovations.agent.config;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
 
 import javax.annotation.Nonnull;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -17,6 +27,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class RedisCacheConfig {
+    @Value("${REDIS}")
+    private String redisUrl;
+
+    private static final String PASSWORD_SECRET = "/run/secrets/redis_password";
+
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
         ObjectMapper om = new ObjectMapper();
@@ -38,5 +53,23 @@ public class RedisCacheConfig {
                 .serializeValuesWith(
                         RedisSerializationContext.SerializationPair
                                 .fromSerializer(jackson2JsonRedisSerializer));
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        try {
+            URI uri = new URI(redisUrl);
+            String host = uri.getHost();
+            int port = (uri.getPort() != -1) ? uri.getPort() : 6379;
+
+            RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+            String password = Files.readString(Paths.get(PASSWORD_SECRET)).trim();
+            config.setPassword(password);
+            return new LettuceConnectionFactory(config, LettuceClientConfiguration.defaultConfiguration());
+        } catch (IOException e) {
+            throw new UncheckedIOException("Failed to read Redis secret file", e);
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException("Invalid Redis URL: " + redisUrl, e);
+        }
     }
 }
