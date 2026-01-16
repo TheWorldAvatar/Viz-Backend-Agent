4e922a1a35b5ec1383534d1b325ec8fb6cb907ea
qhouyee <quekhouyee@gmail.com>
2026-01-16 13:55:19 +0000

    qhouyee/147/instance-changelogs: removed unused variables

--- agent/src/main/java/com/cmclinnovations/agent/service/UpdateService.java
+++ agent/src/main/java/com/cmclinnovations/agent/service/UpdateService.java
@@ -13,7 +13,6 @@ import com.cmclinnovations.agent.component.LocalisationTranslator;
 import com.cmclinnovations.agent.component.ResponseEntityBuilder;
 import com.cmclinnovations.agent.model.response.StandardApiResponse;
 import com.cmclinnovations.agent.model.type.TrackActionType;
-import com.cmclinnovations.agent.service.core.ChangelogService;
 import com.cmclinnovations.agent.service.core.KGService;
 import com.cmclinnovations.agent.utils.LocalisationResource;
 import com.cmclinnovations.agent.utils.QueryResource;
@@ -22,7 +21,6 @@ import com.cmclinnovations.agent.utils.QueryResource;
 public class UpdateService {
   private final AddService addService;
   private final DeleteService deleteService;
-  private final ChangelogService changelogService;
   private final KGService kgService;
   private final ResponseEntityBuilder responseEntityBuilder;
 
@@ -33,15 +31,13 @@ public class UpdateService {
    * 
    * @param addService            KG service to add instances.
    * @param deleteService         KG service to delete instances.
-   * @param changelogService      Service to log changes.
    * @param kgService             KG service for performing the query.
    * @param responseEntityBuilder A component to build the response entity.
    */
-  public UpdateService(AddService addService, DeleteService deleteService, ChangelogService changelogService,
-      KGService kgService, ResponseEntityBuilder responseEntityBuilder) {
+  public UpdateService(AddService addService, DeleteService deleteService, KGService kgService,
+      ResponseEntityBuilder responseEntityBuilder) {
     this.addService = addService;
     this.deleteService = deleteService;
-    this.changelogService = changelogService;
     this.kgService = kgService;
     this.responseEntityBuilder = responseEntityBuilder;
   }
@@ -66,9 +62,7 @@ public class UpdateService {
       return this.addService.instantiate(resourceId, id, editedParams,
           MessageFormat.format("{0} has been successfully updated for {1}", resourceId, id), successMessageId,
           trackAction);
-    } else
-
-    {
+    } else {
       return deleteResponse;
     }
   }
@@ -89,5 +83,4 @@ public class UpdateService {
         LocalisationTranslator.getMessage(LocalisationResource.ERROR_INVALID_SERVER_KEY),
         HttpStatus.INTERNAL_SERVER_ERROR);
   }
-
 }
