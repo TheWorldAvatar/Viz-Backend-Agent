aa3290f84ddb7d1bd75b98f75326886d35df19b0
ChungTingLao <chung.ting.lao.34@gmail.com>
2026-01-27 13:27:52 +0000

    ctlao/157-cannot-reschedule-single-service: for reschedule task, accept a date instead of timestamp.

--- agent/src/main/java/com/cmclinnovations/agent/service/application/LifecycleTaskService.java
+++ agent/src/main/java/com/cmclinnovations/agent/service/application/LifecycleTaskService.java
@@ -644,8 +644,7 @@ public class LifecycleTaskService {
     String expireStage = result.getFieldValue(QueryResource.genVariable(LifecycleResource.STAGE_KEY).getVarName());
     String orderEvent = result.getFieldValue(QueryResource.genVariable(LifecycleResource.EVENT_KEY).getVarName());
     // new date
-    String rescheduleTimestamp = params.get(LifecycleResource.RESCHEDULE_DATE_KEY).toString();
-    String rescheduleDate = this.dateTimeService.getDateFromTimestamp(rescheduleTimestamp);
+    String rescheduleDate = params.get(LifecycleResource.RESCHEDULE_DATE_KEY).toString();
     String rescheduleDatetime = this.dateTimeService.getDateTimeFromDate(rescheduleDate);
     // update database
     String query = this.lifecycleQueryFactory.getRescheduleQuery(lifecycleStartDate, lifecycleEndDate, expireStage,
