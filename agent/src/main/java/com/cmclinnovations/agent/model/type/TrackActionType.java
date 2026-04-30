8d2cc0d2743081d116a7a08fc8266be20d6bed31
qhouyee <quekhouyee@gmail.com>
2026-04-30 10:51:12 +0100

    qhouyee/243/changelog-translations: refactor to create different activity classes to provided translated messages instead of relying of description string literal

--- agent/src/main/java/com/cmclinnovations/agent/model/type/TrackActionType.java
+++ agent/src/main/java/com/cmclinnovations/agent/model/type/TrackActionType.java
@@ -1,28 +1,55 @@
 package com.cmclinnovations.agent.model.type;
 
+import com.cmclinnovations.agent.component.LocalisationTranslator;
+import com.cmclinnovations.agent.model.SparqlResponseField;
+import com.cmclinnovations.agent.utils.QueryResource;
+import com.cmclinnovations.agent.utils.ShaclResource;
+
 public enum TrackActionType {
-  CREATION("Created"),
-  MODIFICATION("Modified"),
-  CONTRACT_RESET_STATUS("Verified with client; Pending approval"),
-  APPROVED("Approved"),
-  ASSIGNMENT("Assigned"),
-  CANCELLATION("Cancelled"),
-  COMPLETION("Completed"),
-  SAVED_COMPLETION("Saved completion details"),
-  ISSUE_REPORT("Reported issue"),
-  RESCINDMENT("Rescinded"),
-  RESCHEDULED("Rescheduled"),
-  ACCRUAL("Accrued billables"),
-  EXEMPT("Task exempted from billing"),
-  IGNORED("");
+  CREATION("https://theworldavatar.io/kg/ontochangelog/CreationActivity", "label.activity.creation"),
+  MODIFICATION("https://theworldavatar.io/kg/ontochangelog/ModificationActivity", "label.activity.modify"),
+  CONTRACT_RESET_STATUS("https://theworldavatar.io/kg/ontochangelog/ContractResetActivity",
+      "label.activity.reset.contract"),
+  APPROVED("https://theworldavatar.io/kg/ontochangelog/ApprovalActivity", "label.activity.approve"),
+  ASSIGNMENT("https://theworldavatar.io/kg/ontochangelog/AssignmentActivity", "label.activity.assign"),
+  CANCELLATION("https://theworldavatar.io/kg/ontochangelog/CancellationActivity", "label.activity.cancel"),
+  COMPLETION("https://theworldavatar.io/kg/ontochangelog/CompletionActivity", "label.activity.complete"),
+  SAVED_COMPLETION("https://theworldavatar.io/kg/ontochangelog/SavingActivity", "label.activity.saved"),
+  ISSUE_REPORT("https://theworldavatar.io/kg/ontochangelog/IssueReportedActivity", "label.activity.report"),
+  RESCINDMENT("https://theworldavatar.io/kg/ontochangelog/RescindmentActivity", "label.activity.rescind"),
+  RESCHEDULED("https://theworldavatar.io/kg/ontochangelog/RescheduleActivity", "label.activity.reschedule"),
+  ACCRUAL("https://theworldavatar.io/kg/ontochangelog/AccrualActivity", "label.activity.accrue"),
+  EXEMPT("https://theworldavatar.io/kg/ontochangelog/ExemptionActivity", "label.activity.exempt"),
+  IGNORED("", "");
 
-  private final String message;
+  private final String clazz;
+  private final String translationKey;
 
-  TrackActionType(String message) {
-    this.message = message;
+  TrackActionType(String clazz, String translationKey) {
+    this.clazz = clazz;
+    this.translationKey = translationKey;
   }
 
-  public String getMessage() {
-    return this.message;
+  public String getClazz() {
+    return this.clazz;
+  }
+
+  public String getTranslationKey() {
+    return this.translationKey;
+  }
+
+  /**
+   * Gets the corresponding translated message for a given URI.
+   * 
+   * @param uri The activity URI class
+   */
+  public static SparqlResponseField getMessage(String uri) {
+    for (TrackActionType type : TrackActionType.values()) {
+      if (type.clazz.equals(uri)) {
+        String translation = LocalisationTranslator.getMessage(type.getTranslationKey());
+        return new SparqlResponseField(QueryResource.LITERAL_TYPE, translation, ShaclResource.XSD_STRING, "");
+      }
+    }
+    throw new IllegalArgumentException("Invalid URI: " + uri);
   }
 }
