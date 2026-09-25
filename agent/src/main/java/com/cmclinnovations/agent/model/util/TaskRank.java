e5f34b43f34f65289f5d3f90878cdd12615dbd2c
qhouyee <quekhouyee@gmail.com>
2026-09-25 12:31:11 +0100

    qhouyee/347/lexorank-functionality: refactor to make literals more robust

--- agent/src/main/java/com/cmclinnovations/agent/model/util/TaskRank.java
+++ agent/src/main/java/com/cmclinnovations/agent/model/util/TaskRank.java
@@ -1,10 +1,13 @@
 package com.cmclinnovations.agent.model.util;
 
+import org.eclipse.rdf4j.sparqlbuilder.rdf.Rdf;
+
 /**
  * Stores the task ranks
  */
 public record TaskRank(String id, String lexorank) {
     public String getValueClauseValue() {
-        return "(\"" + this.id() + "\" \"" + this.lexorank() + "\")";
+        return "(" + Rdf.literalOf(this.id()).getQueryString() + " " + Rdf.literalOf(this.lexorank()).getQueryString()
+                + ")";
     }
 }
